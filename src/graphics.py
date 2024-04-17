704e763169fbfb6c1218060d934c5d3a0f221ff1
David Shaw <dshaw11@utk.edu>
2024-04-17 15:37:01 -0400

    added more cells, adjusted the imports a bit

--- src/graphics.py
+++ src/graphics.py
@@ -47,18 +47,22 @@ class Line:
 
 
 class Cell:
-    def __init__(self, top_left, bottom_right, window):
+    def __init__(self, window):
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
-        self._x1 = top_left.x
-        self._y1 = top_left.y
-        self._x2 = bottom_right.x
-        self._y2 = bottom_right.y
+        self._x1 = None
+        self._y1 = None
+        self._x2 = None
+        self._y2 = None
         self._win = window
     
-    def draw(self):        
+    def draw(self, x1, y1, x2, y2):        
+        self._x1 = x1
+        self._y1 = y1
+        self._x2 = x2
+        self._y2 = y2
         if self.has_left_wall:
             wall = Line(Point(self._x1, self._y1), Point(self._x1, self._y2))
             self._win.draw_line(wall, "black")
--- src/main.py
+++ src/main.py
@@ -2,8 +2,21 @@ from graphics import Window, Line, Point, Cell
 
 def main():
     win = Window(800, 600)
-    cell_1 = Cell(Point(100,100), Point(200,200), win)
-    cell_1.draw()
+    c = Cell(win)
+    c.has_left_wall = False
+    c.draw(50, 50, 100, 100)
+
+    c = Cell(win)
+    c.has_right_wall = False
+    c.draw(125, 125, 200, 200)
+
+    c = Cell(win)
+    c.has_bottom_wall = False
+    c.draw(225, 225, 250, 250)
+
+    c = Cell(win)
+    c.has_top_wall = False
+    c.draw(300, 300, 500, 500)
     win.wait_for_close()
 
 main()
