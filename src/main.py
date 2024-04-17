c7dcf242117f6165baf243c087cfc97d3ba17357
David Shaw <dshaw11@utk.edu>
2024-04-17 15:33:19 -0400

    removed commented out code

--- src/main.py
+++ src/main.py
@@ -4,7 +4,6 @@ def main():
     win = Window(800, 600)
     cell_1 = Cell(Point(100,100), Point(200,200), win)
     cell_1.draw()
-    # win.draw_line(line1, 'black')
     win.wait_for_close()
 
 main()
