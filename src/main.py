2a61284771b0f9554f754018d5d761b7f2d395ef
David Shaw <dshaw11@utk.edu>
2024-04-17 14:50:07 -0400

    now able to draw a line

--- src/main.py
+++ src/main.py
@@ -2,13 +2,10 @@ from graphics import Window, Line, Point
 
 def main():
     win = Window(800, 600)
-    win.wait_for_close()
-    point1 = Point(0, 10)
-    point2 = Point(10, 1)
-    point3 = Point(2, 20)
-    point4 = Point(20, 2)
+    point1 = Point(0, 100)
+    point2 = Point(100, 1)
     line1 = Line(point1, point2)
-    line2 = Line(point3, point4)
     win.draw_line(line1, 'black')
+    win.wait_for_close()
 
 main()
