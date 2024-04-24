7e3dc60b606226c19b17213f78d1ee0f743b72cf
David Shaw <dshaw11@utk.edu>
2024-04-24 08:19:00 -0400

    working on fixing the DFS

--- src/maze.py
+++ src/maze.py
@@ -30,8 +30,8 @@ class Maze:
         self._break_entrance_and_exit()
         self._break_walls_r(0,0)
         self._reset_cells_visited()
-        self.solve()
-        
+
+
     def _create_cells(self):
         for i in range(self._num_cols):
             temp = []
@@ -126,29 +126,32 @@ class Maze:
     def _solve_r(self,i, j):
         self._animate()
         self._cells[i][j]._visited = True
-        if i > 0 and self._cells[i][j].has_left_wall == False:
-            self._cells[i][j].draw_move(self._cells[i-1][j])
-            if self._cells[i - 1][j]._solve_r(i - 1, j) == True:
+        if self._cells[i][j] == self._cells[-1][-1]:
+            return True
+        
+        if i >= 0 and self._cells[i][j].has_left_wall == False:
+            self._cells[i][j].draw_move(self._cells[i - 1][j])
+            if self._solve_r(i - 1, j) == True:
                 return True
             self._cells[i][j].draw_move(self._cells[i - 1][j], True) 
 
         if i < self._num_cols - 1 and self._cells[i][j].has_right_wall == False:
             self._cells[i][j].draw_move(self._cells[i + 1][j])
-            if self._cells[i + 1][j]._solve_r(i + 1, j) == True:
+            if self._solve_r(i + 1, j) == True:
                 return True
             self._cells[i][j].draw_move(self._cells[i + 1][j], True) 
         
-        if j > 0 and self._cells[i][j].has_top_wall == False:
+        if j >= 0 and self._cells[i][j].has_top_wall == False:
             self._cells[i][j].draw_move(self._cells[i][j - 1])
-            if self._cells[i][j - 1]._solve_r(i, j - 1) == True:
+            if self._solve_r(i, j - 1) == True:
                 return True
             self._cells[i][j].draw_move(self._cells[i][j - 1], True)
 
-        if j < self._num_rows and self._cells[i][j].has_bottom_wall == False:
+        if j < self._num_rows - 1 and self._cells[i][j].has_bottom_wall == False:
             self._cells[i][j].draw_move(self._cells[i][j + 1])
-            if self._cells[i][j + 1]._solve_r(i, j + 1) == True:
+            if self._solve_r(i, j + 1) == True:
                 return True
             self._cells[i][j].draw_move(self._cells[i][j + 1], True)
 
-
+        return False
 
